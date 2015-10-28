f250d4db2de5683ff3517e47525af5a62b9f2d34
Ricardo Henrique <ricardohsd@gmail.com>
2015-10-28 22:36:05 +0100

    Fix bug when trying to login with empty user

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -11,10 +11,15 @@ defmodule Pxblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    user = Repo.get_by(User, username: user_params["username"])
+  def create(conn, %{"user" => %{"username" => username,
+    "password" => password}}) when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
     user
-    |> sign_in(user_params["password"], conn)
+    |> sign_in(password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
@@ -25,9 +30,7 @@ defmodule Pxblog.SessionController do
   end
 
   defp sign_in(user, password, conn) when is_nil(user) do
-    conn
-    |> put_flash(:error, "Invalid username/password combination!")
-    |> redirect(to: page_path(conn, :index))
+    failed_login(conn)
   end
 
   defp sign_in(user, password, conn) do
@@ -37,10 +40,15 @@ defmodule Pxblog.SessionController do
       |> put_flash(:info, "Sign in successful!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
+  end
+
+  defp failed_login(conn) do
+    conn
+    |> put_session(:current_user, nil)
+    |> put_flash(:error, "Invalid username/password combination!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt()
   end
 end
